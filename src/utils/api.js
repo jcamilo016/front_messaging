4e049dffbbe2d09b78f492aa37adb18558141302
Juan Camilo Acosta <jcamilo016@gmail.com>
2021-09-27 12:39:44 -0500

    Change backend api url to https

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: `http://ec2-35-175-138-59.compute-1.amazonaws.com:8443/`,
+    baseURL: `https://ec2-35-175-138-59.compute-1.amazonaws.com:8443/`,
 });
