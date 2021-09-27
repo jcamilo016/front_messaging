8f59c878d0dd197d24236f876fd17e391ec63254
Juan Camilo Acosta <jcamilo016@gmail.com>
2021-09-27 03:07:08 -0500

    first commit

---
+++ src/components/Menu.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useCallback } from "react";
+import SwipeableViews from 'react-swipeable-views';
+import { useTheme, Box, AppBar, Tabs, Tab } from "@mui/material";
+import TabPanel from "./TabPanel";
+
+function Menu(props) {
+    const theme = useTheme();
+    const [value, setValue] = useState(0);
+
+    const a11yProps = useCallback((index) => (
+        {
+            id: `full-width-tab-${index}`,
+            'aria-controls': `full-width-tabpanel-${index}`,
+        }
+    ), []);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const handleChangeIndex = (index) => {
+        setValue(index);
+    };
+
+    return (
+        <Box sx={{ bgcolor: 'background.paper', width: 'auto' }}>
+            <AppBar position="static">
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="secondary"
+                    textColor="inherit"
+                    variant="fullWidth"
+                    aria-label="full width tabs example"
+                >
+                    <Tab label="Mensajería" {...a11yProps(0)} />
+                    <Tab label="Destinatarios" {...a11yProps(1)} />
+                </Tabs>
+            </AppBar>
+            <SwipeableViews
+                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                index={value}
+                onChangeIndex={handleChangeIndex}
+            >
+                <TabPanel value={value} index={0} dir={theme.direction}>
+                    Info Mensajeria
+                </TabPanel>
+                <TabPanel value={value} index={1} dir={theme.direction}>
+                    Info Destinatarios
+                </TabPanel>
+                <TabPanel value={value} index={2} dir={theme.direction}>
+                    Item Three
+                </TabPanel>
+            </SwipeableViews>
+        </Box>
+    );
+}
+
+export default Menu;
---
+++ src/components/TabPanel.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+function TabPanel(props) {
+    const { children, value, index, ...rest } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`full-width-tabpanel-${index}`}
+            aria-labelledby={`full-width-tab-${index}`}
+            {...rest}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+export default TabPanel;
